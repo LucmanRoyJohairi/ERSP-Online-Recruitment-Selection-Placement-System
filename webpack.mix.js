99cc5faab0948bc0f1774f4a79c2fc60d7d39209
Roy Johairi Lucman <lucman.royjohairi@gmail.com>
2022-06-27 13:29:33 +0800

    added jquery configuration

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,4 +1,4 @@
-const mix = require('laravel-mix');
+const mix = require("laravel-mix");
 
 /*
  |--------------------------------------------------------------------------
@@ -10,7 +10,19 @@ const mix = require('laravel-mix');
  | file for the application as well as bundling up all the JS files.
  |
  */
+mix.webpackConfig((webpack) => {
+	return {
+		plugins: [
+			new webpack.ProvidePlugin({
+				$: "jquery",
+				jQuery: "jquery",
+				"window.jQuery": "jquery",
+			}),
+		],
+	};
+});
 
-mix.js('resources/js/app.js', 'public/js')
-    .sass('resources/sass/app.scss', 'public/css')
-    .sourceMaps();
+mix
+	.js("resources/js/app.js", "public/js")
+	.sass("resources/sass/app.scss", "public/css")
+	.sourceMaps();
