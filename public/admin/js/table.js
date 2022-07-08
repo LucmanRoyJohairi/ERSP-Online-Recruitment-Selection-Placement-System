73e25ec0a0ab61efe2f21e494770ebeede73a7ed
Roy Johairi Lucman <lucman.royjohairi@gmail.com>
2022-07-08 15:32:58 +0800

    used datatable

--- public/admin/js/table.js
+++ public/admin/js/table.js
@@ -3,48 +3,56 @@
 // } );
 
 $(function () {
-    // $("#user-table").DataTable();
+	// $("#user-table").DataTable();
 
-    console.log("ger");
-    $("#user-table").DataTable({
-        bPaginate: true,
-        bLengthChange: true,
-        bFilter: true,
-        bInfo: false,
-        bAutoWidth: true,
-    });
+	// console.log("ger");
+	$("#user-table").DataTable({
+		bPaginate: true,
+		bLengthChange: true,
+		bFilter: true,
+		bInfo: false,
+		bAutoWidth: true,
+	});
 
-    $("#manage-table").DataTable({
-        bPaginate: true,
-        bLengthChange: true,
-        bFilter: true,
-        bInfo: false,
-        bAutoWidth: true,
-    });
+	$(".applicants-table").DataTable({
+		bPaginate: true,
+		bLengthChange: true,
+		bFilter: true,
+		bInfo: false,
+		bAutoWidth: true,
+	});
 
-    $("#job-offer-tbl").DataTable({
-        bPaginate: true,
-        bLengthChange: true,
-        bFilter: true,
-        bInfo: false,
-        bAutoWidth: true,
-    });
+	$("#manage-table").DataTable({
+		bPaginate: true,
+		bLengthChange: true,
+		bFilter: true,
+		bInfo: false,
+		bAutoWidth: true,
+	});
 
-    $("#applications-tbl").DataTable({
-        bPaginate: true,
-        bLengthChange: true,
-        bFilter: true,
-        bInfo: false,
-        bAutoWidth: true,
-    });
+	$("#job-offer-tbl").DataTable({
+		bPaginate: true,
+		bLengthChange: true,
+		bFilter: true,
+		bInfo: false,
+		bAutoWidth: true,
+	});
 
-    $("#issuance-tbl").DataTable({
-        bPaginate: true,
-        bLengthChange: true,
-        bFilter: true,
-        bInfo: false,
-        bAutoWidth: true,
-    });
+	$("#applications-tbl").DataTable({
+		bPaginate: true,
+		bLengthChange: true,
+		bFilter: true,
+		bInfo: false,
+		bAutoWidth: true,
+	});
+
+	$("#issuance-tbl").DataTable({
+		bPaginate: true,
+		bLengthChange: true,
+		bFilter: true,
+		bInfo: false,
+		bAutoWidth: true,
+	});
 });
 
 // $(document).ready(function () {
--- resources/views/admin/applicants/index.blade.php
+++ resources/views/admin/applicants/index.blade.php
@@ -18,7 +18,7 @@
                         <nav >
                           <div class="nav nav-tabs" id="nav-tab" role="tablist">
                             <a class="nav-link active" id="nav-home-tab" data-bs-toggle="tab" href="#nav-home" role="tab" aria-controls="nav-home" aria-selected="true">ACTIVE</a>
-                            <a class="nav-link" id="nav-profile-tab" data-bs-toggle="tab" href="#nav-profile" role="tab" aria-controls="nav-profile" aria-selected="false">INACTIVE ( Hired )</a>
+                            <a class="nav-link" id="nav-profile-tab" data-bs-toggle="tab" href="#nav-profile" role="tab" aria-controls="nav-profile" aria-selected="false">HIRED</a>
                           </div>
                         </nav>
                       <div>
@@ -26,19 +26,19 @@
                         <div class="tab-content" id="nav-tabContent">
                           <div class="tab-pane fade show active" id="nav-home" role="tabpanel" aria-labelledby="nav-home-tab">
                           <div class="table-responsive mt-3">
-                                    <table class="table table-applicants">
-                                      <thead class="bg-primary">
+                                    <table class="table table-applicants applicants-table">
+                                      <thead>
                                         <tr>
-                                          <th class="text-left text-white">ID</th>
-                                          <th class="text-white">FIRST NAME</th>
-                                          <th class="text-white">LAST NAME</th>
-                                          <th class="text-white">DATE OF BIRTH</th>
-                                          <th class="text-white">CONTACT</th>
-                                          <th class="text-white">EMAIL</th>
-                                          <th class="text-white">JOB APPLIED</th>
-                                          <th class="text-white">DATE OF APPLICATION</th>
-                                          <th class="text-white"> STATUS</th>
-                                          <th class="text-white">ACTION</th>
+                                          <th class="text-left ">ID</th>
+                                          <th >FIRST NAME</th>
+                                          <th >LAST NAME</th>
+                                          <th >DATE OF BIRTH</th>
+                                          <th >CONTACT</th>
+                                          <th >EMAIL</th>
+                                          <th >JOB APPLIED</th>
+                                          <th >DATE OF APPLICATION</th>
+                                          <th > STATUS</th>
+                                          <th >ACTION</th>
                                         </tr>
                                       </thead>
                                       <tbody>
@@ -96,19 +96,19 @@
                           </div>
                           <div class="tab-pane fade" id="nav-profile" role="tabpanel" aria-labelledby="nav-profile-tab">
                           <div class="table-responsive mt-3">
-                                      <table class="table ">
-                                        <thead class="bg-primary">
+                                      <table class="table applicants-table">
+                                        <thead>
                                           <tr>
-                                            <th class="text-left text-white">ID</th>
-                                            <th class="text-white">FIRST NAME</th>
-                                            <th class="text-white">LAST NAME</th>
-                                            <th class="text-white">DATE OF BIRTH</th>
-                                            <th class="text-white">CONTACT</th>
-                                            <th class="text-white">EMAIL</th>
-                                            <th class="text-white">JOB APPLIED</th>
-                                            <th class="text-white">DATE OF APPLICATION</th>
-                                            <th class="text-white"> STATUS</th>
-                                            <th class="text-white">ACTION</th>
+                                            <th class="text-left">ID</th>
+                                            <th>FIRST NAME</th>
+                                            <th>LAST NAME</th>
+                                            <th>DATE OF BIRTH</th>
+                                            <th>CONTACT</th>
+                                            <th>EMAIL</th>
+                                            <th>JOB APPLIED</th>
+                                            <th>DATE OF APPLICATION</th>
+                                            <th> STATUS</th>
+                                            <th>ACTION</th>
                                           </tr>
                                         </thead>
                                         <tbody>
